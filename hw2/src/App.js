cb757b5beb20aa504ca770bbc1f6abe81db718e2
Mu2Almalki <Mushira.Almalki.ruh.js@tuwaiq.edu.sa>
2021-10-27 02:30:46 +0300

    add commit

---
+++ hw2/src/App.js
@@ -0,0 +1,94 @@
+import { Component } from "react";
+import './index.css';
+
+
+  class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      newItem:"",
+      list:[],
+      count:0
+    }
+  }
+
+  updateInput(key,value){
+    //update react state 
+    this.setState({
+      [key]:value
+    })
+  }
+   incrementCount =()=>{
+     this.setState({
+      count:this.state.count +1
+     })
+    }
+     decrementCount =()=>{
+      this.setState({
+       count:this.state.count -1 
+      })
+     
+   }
+  addItem(){
+    // creat item with unique id 
+    const newItem ={
+      id:1+Math.random(),
+      value:this.state.newItem.slice()
+    };
+    // copy of current list of items
+    const list =[...this.state.list]
+    // add new item to list
+    list.push(newItem);
+
+    // update state with new list and reset newItem input
+    this.setState({
+      list,
+      newItem:""
+    })
+  };
+   deleteItem(id){
+    const list=[...this.state.list];
+    const updatedList = list.filter(item=>item.id !==id);
+    this.setState({list:updatedList});
+  }
+ render() {
+   let {count}=this.state;
+    return (
+      <div className="App">
+        <div>
+        <h1>What needs to be done</h1>
+          <br/>
+          <input
+             type="text"
+              placeholder="Add ToDo"
+              value={this.state.newItem}
+             onChange={e=>this.updateInput("newItem",e.target.value)}
+  
+           />
+           <button onClick={()=>this.addItem()}>
+              Add
+           </button>
+           <br/>
+           <ul>
+             {this.state.list.map(item=>{
+               return(
+                 <li key={item.id}>
+                   {item.value}
+                   <button onClick={()=>this.deleteItem(item.id)}> Delete </button>
+                   <br/>
+                   <button title="+" task={()=> this.incrementCount()}>+</button>
+                   <h4>{count} </h4>
+                   <button title="-" task={()=> this.decrementCount()}>-</button>
+                 </li>
+               )
+             })}
+           </ul>
+        </div>
+      </div>
+    )
+  }
+  }
+export default App;
+
+ 
+     
---
+++ hw2/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+function Header(){
+    return(
+<div className="header">
+ <h1>To Do</h1>
+ </div>
+    )}
+export default Header;
